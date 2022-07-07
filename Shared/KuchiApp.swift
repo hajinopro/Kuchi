438a1541c29119f70366aff807d6c671669b226c
hajinopro <hajinopro@gmail.com>
2022-07-07 21:09:29 +0900

    Initial Commit

---
+++ Shared/KuchiApp.swift
@@ -0,0 +1,17 @@
+//
+//  KuchiApp.swift
+//  Shared
+//
+//  Created by 하진호 on 2022/07/07.
+//
+
+import SwiftUI
+
+@main
+struct KuchiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
